2528b8c594c49cb69bb8d30023f5e0d99e665f43
Rebecca Blanton <beccaablanton@gmail.com>
2021-01-06 04:21:45 -0800

    set up new connection for jawsDB to host on heroku

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,14 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: process.env.PORT || 3306,
-  user: "root",
-  password: "Platypus3!",
-  database: "pokemon_db"
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
 });
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
-
+connection.end();
 module.exports = connection;
