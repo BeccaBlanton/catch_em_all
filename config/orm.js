4f8dcbb6f6ad8fdf731cb42bbe46217f8bb9b2b7
Rebecca Blanton <beccaablanton@gmail.com>
2021-01-05 16:17:53 -0800

    modified ORM to simplify code and to set new entries with a caught value of false

--- config/orm.js
+++ config/orm.js
@@ -8,13 +8,26 @@ const ORM = {
         cb(res);
     });
     },
-    insertOne:(table, column, value, cb)=>{
-    var query = "INSERT INTO ?? SET ?? = ?";
-    connection.query(query, [table, column, value], function(err, res){
+    insertOne: function(table, cols, vals, cb) {
+        var queryString = `INSERT INTO ${table} (${cols}, caught) VALUES ("${vals}", false)`
+        console.log(queryString);
+    
+        connection.query(queryString, vals, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+      },
+    /*insertOne:(table, keyValues, cb)=>{
+    var query = "INSERT INTO ?? SET ?";
+    connection.query(query, 
+        [table, keyValues], function(err, res){
         if (err) throw err;
         cb(res);
     });
-    },
+    },*/
     updateOne:(table, column, value, col2, val2, cb)=>{
     var query = "UPDATE ?? SET ?? = ? WHERE ??=?";
     connection.query(query, [table, column, value, col2, val2], function(err, res){
