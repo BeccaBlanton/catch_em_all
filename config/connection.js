95a2aa625fe85401f10af319cee10982adcdf84c
Rebecca Blanton <beccaablanton@gmail.com>
2021-01-06 16:16:52 -0800

    modified connection for jawsdb otherwise to use localhost

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,25 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
-
-connection.connect();
-
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
-
-  console.log('The solution is: ', rows[0].solution);
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: process.env.PORT || 3306,
+  user: "root",
+  password: "Platypus3!",
+  database: "pokemon_db"
+});
+};
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
 
-connection.end();
+
 module.exports = connection;
